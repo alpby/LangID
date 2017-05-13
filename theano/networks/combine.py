fe0cb7fbffb76cd6e058215b4b07ecd0fbea9c46
alper b <alperbayram2@gmail.com>
2017-05-13 04:25:27 +0300

    Network cores complete

--- theano/networks/combine.py
+++ theano/networks/combine.py
@@ -5,7 +5,7 @@ import PIL.Image as Image
 
 class Model:
 
-    def __init__(self, dataclass, trndata, tstdata, batchsize, dropout, **kwargs):
+    def __init__(self, dataclass, trndata, tstdata, batchsize, dropout, rnnunits, **kwargs):
         def convpool(model,filters,filterSize,stride,pooling,poolingStride,padding):
             model = lasagne.layers.Conv2DLayer(model,filters,filterSize,stride)
             model = lasagne.layers.MaxPool2DLayer(model,pooling,poolingStride,padding)
@@ -16,6 +16,7 @@ class Model:
         self.tstdata = tstdata
         self.batchsize = batchsize
         self.dropout = dropout
+        self.units = rnnunits
         self.dataclass = dataclass
 
         self.spectrograms = theano.tensor.tensor4('spectrograms')
@@ -25,28 +26,41 @@ class Model:
 
         model = convpool(model,16,(7,7),1,(3,3),2,2)
         model = convpool(model,32,(5,5),1,(3,3),2,2)
-        model = convpool(model,64,(3,3),1,(3,3),2,2)
-        model = convpool(model,128,(3,3),1,(3,3),2,2)
-        model = convpool(model,128,(3,3),1,(3,3),2,2)
-        model = convpool(model,256,(3,3),1,(3,3),(3,2),2)
+        model = convpool(model,32,(3,3),1,(3,3),2,2)
+        model = convpool(model,32,(3,3),1,(3,3),2,2)
 
-        model = lasagne.layers.DenseLayer(model, 1024)
+        self.params = lasagne.layers.get_all_params(model, trainable=True)
+
+        output = lasagne.layers.get_output(model)
+        output = output.transpose((0, 3, 1, 2))
+        output = output.flatten(ndim=3)
+
+        # NOTE: these constants are shapes of last pool layer, it can be symbolic
+        # explicit values are better for optimizations
+        num_channels = 64
+        filter_W = 54
+        filter_H = 8
+
+        # InputLayer
+        model = lasagne.layers.InputLayer((None, filter_W, num_channels * filter_H), output)
+        # GRULayer
+        model = lasagne.layers.GRULayer(model, self.units, only_return_final=True)
         model = lasagne.layers.BatchNormLayer(model)
         if (self.dropout > 0):
             model = lasagne.layers.dropout(model, self.dropout)
 
+        # Last layer: classification
         model = lasagne.layers.DenseLayer(model, self.dataclass, nonlinearity=lasagne.nonlinearities.softmax)
 
-        self.params = lasagne.layers.get_all_params(model, trainable=True)
+        self.params += lasagne.layers.get_all_params(model, trainable=True)
         self.prediction = lasagne.layers.get_output(model)
 
         self.loss = lasagne.objectives.categorical_crossentropy(self.prediction, self.langs).mean()
-        updates = lasagne.updates.adam(self.loss, self.params, learning_rate=0.003)
-        # TODO: L2 regularization may be applied here
-        # Try with different optimizers
 
-        self.train_fn = theano.function([self.spectrograms,self.langs],[self.prediction,self.loss],updates=updates)
-        self.test_fn = theano.function([self.spectrograms,self.langs],[self.prediction,self.loss])
+        updates = lasagne.updates.adam(self.loss, self.params, learning_rate=0.003)
+
+        self.train_fn = theano.function([self.spectrograms, self.langs],[self.prediction, self.loss],updates=updates)
+        self.test_fn = theano.function([self.spectrograms,self.langs],[self.prediction, self.loss])
 
     def step(self, batch, mode):
         def read_batch(specs, batch):
